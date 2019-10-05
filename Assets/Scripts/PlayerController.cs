855dc488eb1a34ad9fc923a937f4ed466cfa7e33
Jesse Mendoza <33880932+jessmend815@users.noreply.github.com>
2019-10-05 16:02:29 -0700

    Player Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 movement;           
+    private Rigidbody2D rb;  
+    public float movementSpeed = 1f;   
+    
+    public Sprite[] costumes;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+    }
+}
